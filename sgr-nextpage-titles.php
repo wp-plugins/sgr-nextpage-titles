335a6efa6af0542e3330129a726378500804550a
SGr33n <SGr33n@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-28 22:03:27 +0000

    Fully tested on WordPress 4.1 and bugfixes
    git-svn-id: https://plugins.svn.wordpress.org/sgr-nextpage-titles/trunk@1055886 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sgr-nextpage-titles.php
+++ sgr-nextpage-titles.php
@@ -5,7 +5,7 @@ Plugin Name: Multipage Plugin
 Plugin URI: http://wordpress.org/plugins/sgr-nextpage-titles/
 Description: Multipage Plugin for WordPress (formerly sGR Nextpage Titles) will give you the ability to order a post in multipages, giving each subpage a title and having a table of contents.
 Author: Sergio De Falco aka SGr33n
-Version: 1.2.2
+Version: 1.2.3
 Author URI: http://www.gonk.it/
 */
 
@@ -58,14 +58,6 @@ class Multipage_Plugin_Loader {
 		} else {
 			add_action( 'wp', array( &$this, 'public_init' ) );
 		}
-	}
-	
-	/**
-	 * End
-	 *
-	 * @since 0.6
-	 */
-	public function __deconstruct() {
 	}
 	
 	/**
@@ -261,9 +253,7 @@ class Multipage_Plugin_Loader {
 			add_filter( 'comments_template', array( &$this, 'hide_comments' ) );
 
 		$subpages = $post->post_subpages;
-		$subtitle = '<h2 class="entry-subtitle">' . $subpages['title'][ $page -1 ] . '</h2>';
-		
-		add_filter( 'multipage_subtitle', 'return_multipage_subtitle' );
+		$subtitle = '<h2 class="entry-subtitle">' . apply_filters( 'multipage_subtitle', $subpages['title'][ $page -1 ] ) . '</h2>';
 	
 		if ( $page >= max( array_map( 'count', $subpages ) ) ) {
 			$multipagenav = '<div class="multipage-navlink">' . __( 'Back to: ', 'sgr-npt' ) . ' <a rel="index" href="' . get_permalink() . '">' . $subpages['title'][ 0 ] . '</a></div>';
@@ -271,7 +261,7 @@ class Multipage_Plugin_Loader {
 			$multipagenav = '<div class="multipage-navlink">' . __( 'Continue:', 'sgr-npt' ) . ' <a rel="next" href="' . $this->get_subpage_link( $page +1 ) . '">' . $subpages['title'][ $page ] .'</a></div>';
 		}
 		
-		add_filter( 'multipage_navigation', 'return_multipage_navigation' );
+		$multipagenav = apply_filters( 'multipage_navigation', $multipagenav );
 		
 		$enhanced_content = $subtitle . $content . $multipagenav;
 
@@ -316,11 +306,9 @@ class Multipage_Plugin_Loader {
 			elseif ( $options['toc-position'] === 'bottom' ) {
 				$enhanced_content .= $toc;
 			}
-			
-			add_filter( 'multipage_content', 'return_multipage_content' );
 		}
 
-		return $enhanced_content;
+		return apply_filters( 'multipage_content', $enhanced_content );
 	}
 	
 	/**
@@ -345,36 +333,6 @@ class Multipage_Plugin_Loader {
 			$subpage_link .= "#toc";
 
 		return $subpage_link;
-	}
-
-	/**
-	 * Return Subpage Title.
-	 *
-	 * @since 1.1.1
-	 */
-	public function return_multipage_subtitle( $subtitle ) {
-		
-		return $subtitle;
-	}
-
-	/**
-	 * Return Multipage Navigation.
-	 *
-	 * @since 1.1.1
-	 */
-	public function return_multipage_navigation( $multipagenav ) {
-		
-		return $multipagenav;
-	}
-	
-	/**
-	 * Return Subpage Content.
-	 *
-	 * @since 1.1.1
-	 */
-	public function return_multipage_content( $content ) {
-		
-		return $content;
 	}
 
 	/**
